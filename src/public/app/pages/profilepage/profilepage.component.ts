c7d87eaa1358e2767563cf703f354e263ab3799c
Somma <luisa.somma@accenture.com>
2018-05-23 18:32:43 +0200

    Modify_profile

--- src/public/app/pages/profilepage/profilepage.component.ts
+++ src/public/app/pages/profilepage/profilepage.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-profilepage',
-  templateUrl: './profilepage.component.html',
-  styleUrls: ['./profilepage.component.css']
+  selector: "app-profilepage",
+  templateUrl: "./profilepage.component.html",
+  styleUrls: ["./profilepage.component.css"]
 })
 export class ProfilepageComponent implements OnInit {
+  private myprofile  = {
+      "id": 1,
+      "username": "luisa",
+      "email": "luisa.somma@accenture.com"
+    };
 
+  public disabledField = "true";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.disabledField === "true") {
+      this.disabledField = "false";
+    } else {
+      this.disabledField = "true";
+    }
+
   }
 
 }
--- src/public/app/pages/settingspage/settingspage.component.ts
+++ src/public/app/pages/settingspage/settingspage.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-settingspage',
-  templateUrl: './settingspage.component.html',
-  styleUrls: ['./settingspage.component.css']
+  selector: "app-settingspage",
+  templateUrl: "./settingspage.component.html",
+  styleUrls: ["./settingspage.component.css"]
 })
 export class SettingspageComponent implements OnInit {
-
+  items = [
+    {
+      "id": 1,
+      "icon": "group-icon",
+      "iconName": "group",
+      "nome": "Invita un amico",
+      "routerLink" : ""
+    },
+    {
+      "id": 2,
+      "icon": "help-icon",
+      "iconName": "help",
+      "nome": "Aiuto",
+      "routerLink" : ""
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
