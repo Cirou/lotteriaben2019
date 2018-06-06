0a429fe4df5ddec112b372a5d7a98924c3a5f38c
Cirou <ciroaiello@live.com>
2018-06-06 19:42:36 +0200

    Food Places

--- src/public/app/services/food.service.ts
+++ src/public/app/services/food.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Cibo } from '../../../models/Cibo';
+import { Ristorante } from '../../../models/Ristorante';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 
 import { map, catchError } from 'rxjs/operators';
+import { RiskExceptionConfigurationType } from 'aws-sdk/clients/cognitoidentityserviceprovider';
 
 @Injectable()
 export class FoodService {
@@ -11,6 +13,7 @@ export class FoodService {
   private cibo: Cibo;
   private ciboUrl: string = 'public/assets/mock/getFoodDetails.json'
   private elencoCiboUrl: string = 'public/assets/mock/getFoodList.json'
+  private elencoRistorantiUrl: string = 'public/assets/mock/getFoodPlaces.json'
 
   constructor(private http: Http) { }
 
@@ -25,6 +28,14 @@ export class FoodService {
   getFoodDetails(idCibo:string): Observable<Cibo> {
     return this.http
       .get(this.ciboUrl)
+      .pipe(
+        map((response: Response) => response.json()),
+        catchError((error: any) => Observable.throw(error.json().error || 'Server error')));
+  }
+
+  getFoodPlaces(): Observable<Ristorante[]> {
+    return this.http
+      .get(this.elencoRistorantiUrl)
       .pipe(
         map((response: Response) => response.json()),
         catchError((error: any) => Observable.throw(error.json().error || 'Server error')));
