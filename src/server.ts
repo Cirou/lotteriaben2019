708c2c36d92c329e0dd7457e877e51910c9db767
Cirou <ciroaiello@live.com>
2018-06-11 11:51:18 +0200

    Fix compiler

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ const app = require('./app');
  * Start Express server.
  */
 export const server = app.listen(app.get('port'), () => {
-  console.log(('  App is running at http://localhost:%d in %s mode'), app.get('port'), app.get('env'));
-  console.log('  Press CTRL-C to stop\n');
+
+  console.log('\x1b[36m%s\x1b[0m', 'App is running at http://localhost:' + app.get('port') + ' in ' + app.get('env') + ' mode');
+  console.log('\x1b[36m%s\x1b[0m', 'Press CTRL-C to stop\n');
+
 });
