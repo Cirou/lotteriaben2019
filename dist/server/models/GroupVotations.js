fbb77951398fc16bee3d9ba1a72584b74614f065
Cirou <ciroaiello@live.com>
2018-11-27 17:16:11 +0100

    delete release

--- dist/server/models/GroupVotations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GroupVotations = /** @class */ (function () {
-    function GroupVotations() {
-    }
-    return GroupVotations;
-}());
-exports.GroupVotations = GroupVotations;
