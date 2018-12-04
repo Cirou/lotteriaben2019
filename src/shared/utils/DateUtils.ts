a495f6f5d759dd5dd60f709654eb05af486b5973
Cirou <ciroaiello@live.com>
2018-12-04 15:29:53 +0100

    post-merge

--- src/shared/utils/DateUtils.ts
+++ src/shared/utils/DateUtils.ts
@@ -8,5 +8,5 @@ export function formatDateTime(date) {
 }
 
 export function formatDateTimezone(date) {
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
 }
