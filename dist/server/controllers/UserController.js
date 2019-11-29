af2f004ab74e13f231c28ee1d367e2437bbca5c6
ciro.aiello <ciro.aiello@accenture.com>
2019-11-30 00:59:33 +0100

    release

---
+++ dist/server/controllers/UserController.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * POST /user
+ * saves the premi using the given model
+ */
+exports.postUser = function (req, res) {
+    if (req.body.pwd === 'lotteria2019') {
+        res.send({ id: 'admin1', isValid: true });
+    }
+    res.send({ id: '', isValid: false });
+};
